b2c31b8f86244897aa3018b59abe59285646dadb
martinchiu <martin87713@gmail.com>
2022-02-05 23:10:29 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 
+const routes = require('./routes')
+
 const app = express()
 const PORT = 3000
 
@@ -10,10 +12,8 @@ app.set('view engine', 'hbs')
 require('./config/mongoose')
 
 app.use(express.static('public'))
+app.use(routes)
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
