c0fc5c599dcd3cb8a7dc3637a6c2a5ac82c07058
martinchiu <martin87713@gmail.com>
2022-02-05 22:28:10 +0800

    feat: modify layout

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PORT = 3000
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.render('index')
 })
