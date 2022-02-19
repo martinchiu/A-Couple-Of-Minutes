08056f38681376e1a690a37c7b3a74d825d71897
martinchiu <martin87713@gmail.com>
2022-02-19 11:50:12 +0800

    feat: add game_start page

--- controllers/game_controller.js
+++ controllers/game_controller.js
@@ -1,5 +1,20 @@
 module.exports = {
   getStart: (req, res) => {
-    res.render('game')
+    res.render('game', {
+      style: "game/start"
+    })
+  },
+  getfirstMethod: (req, res) => {
+    const { level } = req.params
+    res.send(`${level}`)
+  }
+  ,
+  getsecondMethod: (req, res) => {
+    const { number } = req.query
+    res.send(`${number}`)
+  },
+  getthirdMethod: (req, res) => {
+    const { topicNumber } = req.params
+    res.send(`${topicNumber}`)
   }
 }
--- routes/modules/game.js
+++ routes/modules/game.js
@@ -3,6 +3,12 @@ const router = express.Router()
 
 const gameController = require('../../controllers/game_controller')
 
+router.get('/firstMethod/:level', gameController.getfirstMethod)
+
+router.get('/secondMethod', gameController.getsecondMethod)
+
+router.get('/thirdMethod/topic/:topicNumber', gameController.getthirdMethod)
+
 router.get('/start', gameController.getStart)
 
 module.exports = router
