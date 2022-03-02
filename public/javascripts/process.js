4459c22a3136e0bcc548ef8f63b38ff5ab41570c
martinchiu <martin87713@gmail.com>
2022-03-02 14:58:41 +0800

    feat: modify nextBtn

--- public/javascripts/process.js
+++ public/javascripts/process.js
@@ -2,6 +2,13 @@ const nextBtn = document.querySelector('.nextBtn')
 const amount = document.querySelector('.number')
 let number = Number(amount.innerHTML)
 nextBtn.addEventListener('click', e => {
+  // 剩餘牌數減一
   number--
   amount.innerHTML = number
+  // 抽牌改成下一張牌
+  nextBtn.innerHTML = `
+  <i class="fas fa-arrow-circle-right icon"></i>
+  <p>下一張牌</p>
+  `
+  
 })
