6d780ea090f6aeeb320123f9412da77b6f17d407
martinchiu <martin87713@gmail.com>
2022-03-04 12:19:27 +0800

    feat: finish game logic

--- public/javascripts/process.js
+++ public/javascripts/process.js
@@ -1,14 +1,27 @@
 const nextBtn = document.querySelector('.nextBtn')
 const amount = document.querySelector('.number')
-let number = Number(amount.innerHTML)
+let conutNumber = Number(amount.innerHTML)
+let initNumber = 0
+const cards = document.querySelectorAll('.card')
 nextBtn.addEventListener('click', e => {
-  // 剩餘牌數減一
-  number--
-  amount.innerHTML = number
-  // 抽牌改成下一張牌
-  nextBtn.innerHTML = `
+  if (initNumber > 0) {
+    // 剩餘牌數減一
+    conutNumber--
+    amount.innerHTML = conutNumber
+    // 換下一張牌
+    cards[initNumber].classList.add('disappear')
+    initNumber++
+  } else {
+    // 抽牌改成下一張牌
+    nextBtn.innerHTML = `
   <i class="fas fa-arrow-circle-right icon"></i>
   <p>下一張牌</p>
   `
-  
+    // 剩餘牌數減一
+    conutNumber--
+    amount.innerHTML = conutNumber
+    //抽牌
+    cards[initNumber].classList.add('disappear')
+    initNumber++
+  }
 })
