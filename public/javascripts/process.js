076d3fe98b35741bb3ee2818c6d71473f708d17c
martinchiu <martin87713@gmail.com>
2022-03-05 07:56:42 +0800

    feat: add pseudo background fot fnCard

--- public/javascripts/process.js
+++ public/javascripts/process.js
@@ -4,6 +4,7 @@ let countNumber = Number(amount.innerHTML)
 let initNumber = 0
 const cards = document.querySelectorAll('.card')
 const crown = document.querySelectorAll('.crownIcon')
+const fnCard = document.querySelectorAll('.fn-card-with-pseudo')
 nextBtn.addEventListener('click', e => {
   // 輪流決定誰抽牌
   crown[0].classList.toggle('invisible')
@@ -16,15 +17,18 @@ nextBtn.addEventListener('click', e => {
     `
     // 隨機決定誰先抽牌
     Math.random() > 0.5 ? crown[0].classList.toggle('invisible') : crown[1].classList.toggle('invisible')
+    // 
+    fnCard.forEach(element => {
+      element.classList.remove('fn-card-with-pseudo')
+      element.classList.add('fn-card')
+    })
   }
   // 剩餘牌數減一
   countNumber--
   amount.innerHTML = countNumber
   //抽牌
   cards[initNumber].classList.add('disappear')
-  initNumber++
-  
-
+  initNumber++ 
   // 抽光牌庫
   if (countNumber === 0) {
     const div = nextBtn.parentElement
