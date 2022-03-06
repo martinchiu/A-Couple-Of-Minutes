45f6e4a3326a2fb5a9f6e4e145237e3beb0ab429
martinchiu <martin87713@gmail.com>
2022-03-06 16:08:11 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const exphbs = require('express-handlebars')
 const routes = require('./routes')
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
